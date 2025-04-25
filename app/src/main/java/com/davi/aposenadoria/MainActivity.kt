a88854fa4b63fbbe08097b48cdd7796d6595a565
davimc0908 <davicalcagno@gmail.com>
2025-04-25 16:33:03 -0300

    Alteração aula 25/04

--- app/src/main/java/com/davi/aposenadoria/MainActivity.kt
+++ app/src/main/java/com/davi/aposenadoria/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.davi.aposenadoria
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.davi.aposenadoria.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val items = listOf("Masculino", "Feminino")
+
+        val adapter = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, items)
+
+        binding.spinner.adapter = adapter
+
+        binding.button.setOnClickListener{
+            val sexoselecionado = binding.spinner.selectedItem as String
+            var idade = binding.edittext.text.toString().toLongOrNull()
+            var  resultado: Long
+            if (idade!= null) {
+                if (sexoselecionado.trim() == "Masculino") {
+                    resultado = 65 - idade
+                }else{
+                    resultado = 62-idade
+                }
+
+                binding.resultado.text = "Faltam $resultado anos para voce se aposentar"
+            }else{
+                binding.resultado.text="Informe sua idade"
+            }
+
+
+
+
         }
+
     }
 }
